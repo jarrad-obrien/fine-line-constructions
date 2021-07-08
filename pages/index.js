a41f7ee2661d179e0272800e63034e3b513e0ced
Roy Duff <Roy@royscleaningservices.co.uk>
2021-07-08 21:40:39 +0200

    Finished mobile fixes

--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,7 @@ export default function Home() {
       </div>
       <div className="what-we-do">
         <div className="what-we-do-left">
-          <h2>WHO ARE WE</h2>
+          <h2>What we do</h2>
           <h1>We Specialise in bringing your vision to life.</h1>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
         </div>
